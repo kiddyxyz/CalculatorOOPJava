2a48ee92c112db0799b7e380790117c3252f9b47
Kiddy <kiddydhana@gmail.com>
2019-04-02 16:21:31 +0700

    Add abstraction class

--- src/mycalculator/Hitung.java
+++ src/mycalculator/Hitung.java
@@ -9,29 +9,50 @@ package mycalculator;
  *
  * @author Kiddy
  */
-public class Hitung {
-   
+class Hitung extends Log {
+  
     public String kali(Operation op){
         float hasil = op.getAngka_pertama() * op.getAngka_kedua();
         String total = hasil + "";
+        
+        // Logging
+        writeLog("kali");
+        
         return total;
     }
     
     public String tambah(Operation op){
         float hasil = op.getAngka_pertama() + op.getAngka_kedua();
         String total = hasil + "";
+        
+        // Logging
+        writeLog("tambah");
+        
         return total;
     }
     
     public String bagi(Operation op){
         float hasil = op.getAngka_pertama() / op.getAngka_kedua();
         String total = hasil + "";
+        
+        // Logging
+        writeLog("bagi");
+        
         return total;
     }
     
     public String kurang(Operation op){
         float hasil = op.getAngka_pertama() - op.getAngka_kedua();
         String total = hasil + "";
+        
+        // Logging
+        writeLog("kurang");
+        
         return total;
+    }
+
+    @Override
+    public void writeLog(String log) {
+        System.out.println("Operasi " + log + " telah dijalankan");
     }
 }
